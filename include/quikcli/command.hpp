a3808c09cba43d9da89c7d4aaf88ea92f33d1053
yiyun-sj <67709970+yiyun-sj@users.noreply.github.com>
2026-05-22 18:52:47 -0700

    ci: create github actions ci.yml (#2)
    * ci: create github actions ci.yml
    
    * fix: apply clang-format
    
    * ci: update clang tidy ci to only run on include/quikcli/* files and apply fixes
    
    * fix: lower cmake min version

--- include/quikcli/command.hpp
+++ include/quikcli/command.hpp
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2026 Yiyun Jia
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -107,7 +107,11 @@ class Command {
              std::ostream &err = std::cerr) const {
         if (argc < 1)
             throw std::invalid_argument("got argc < 1 but expected argv[0] as executable path");
-        RunContext ctx{std::filesystem::path(argv[0]).filename().string(), version, out, err, {}};
+        RunContext ctx{.program_name = std::filesystem::path(argv[0]).filename().string(),
+                       .version = version,
+                       .out = out,
+                       .err = err,
+                       .subcommand_path = {}};
         run_impl(std::span<char *>(argv + 1, argc - 1), ctx);
     }
 
@@ -145,7 +149,7 @@ class Command {
     }
 
     static void run_impl(const Basic &b, std::span<char *> args, RunContext &ctx) {
-        detail::Parser parser(b.specs);
+        const detail::Parser parser(b.specs);
         try {
             auto result = parser.parse(args);
 
@@ -188,7 +192,8 @@ class Command {
         for (const auto &[name, sub] : g.subcommands) {
             if (subcommand == name) {
                 ctx.subcommand_path.emplace_back(name);
-                return sub->run_impl({args.begin() + 1, args.end()}, ctx);
+                sub->run_impl({args.begin() + 1, args.end()}, ctx);
+                return;
             }
         }
         usage_info(std::format("unknown subcommand {}", subcommand), ctx, false);
